f04e3625559d74c7904f47b3511bc83804bfc03c
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-06-16 14:41:58 +0200

    refactor: rewrite setTimeoutCallback() as factory

--- src/factories/set-timer.ts
+++ src/factories/set-timer.ts
@@ -1,12 +1,12 @@
 import type { computeDelayAndExpectedCallbackTime as computeDelayAndExpectedCallbackTimeFunction } from '../functions/compute-delay-and-expected-callback-time';
-import type { setTimeoutCallback as setTimeoutCallbackFunction } from '../functions/set-timeout-callback';
+import type { createSetTimeoutCallback } from './set-timeout-callback';
 import { TResolveSetResponseResultPromise } from '../types';
 
 export const createSetTimer =
     (
         computeDelayAndExpectedCallbackTime: typeof computeDelayAndExpectedCallbackTimeFunction,
         identifiersAndResolvers: Map<number, [number, TResolveSetResponseResultPromise]>,
-        setTimeoutCallback: typeof setTimeoutCallbackFunction
+        setTimeoutCallback: ReturnType<typeof createSetTimeoutCallback>
     ) =>
     (delay: number, nowAndTimeOrigin: number, timerId: number) => {
         const { expected, remainingDelay } = computeDelayAndExpectedCallbackTime(delay, nowAndTimeOrigin);
